1379e3fa68865a1045cc620824fa33d465d3f4bf
Rakhshanipr <you@example.com>
2020-07-23 00:48:25 +0430

    create model Crime.java adding crime for test create disign for first crime

---
+++ app/src/main/java/com/example/criminalintent/model/Crime.java
@@ -0,0 +1,44 @@
+package com.example.criminalintent.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mIsSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mIsSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mIsSolved = solved;
+    }
+
+    public Crime() {
+        mId=UUID.randomUUID();
+        mDate=new Date();
+    }
+}
